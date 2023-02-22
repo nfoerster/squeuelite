72c5131b4be70fa0a3df238ede3e813afabbfc1d
Norman.Foerster <Norman.Foerster@fresenius.com>
2023-02-22 13:47:43 +0100

    added test, switched to lower driver to enable updatehook on connection level

--- squeuelite.go
+++ squeuelite.go
@@ -2,8 +2,10 @@ package squeuelite
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"errors"
 	"fmt"
+	"io"
 	"path/filepath"
 	"time"
 
@@ -11,10 +13,8 @@ import (
 	sqlite3 "github.com/mattn/go-sqlite3"
 )
 
-type MessageStatus int
-
 const (
-	READY MessageStatus = iota
+	READY int64 = iota
 	LOCKED
 	DONE
 	FAILED
@@ -23,55 +23,58 @@ const (
 type Message struct {
 	Data      string
 	MessageID string
-	Status    MessageStatus
+	Status    int64
 	inTime    int64
-	lockTime  *int64
-	doneTime  *int64
+	lockTime  int64
+	doneTime  int64
 }
 
 type SQueueLite struct {
-	conn    *sql.DB
-	maxsize int
-	notify  chan int
+	conn           *sqlite3.SQLiteConn
+	maxsize        int
+	notify         chan int64
+	peekTimeoutSec int
 }
 
 func NewSQueueLite(filenameOrConn string, memory bool, maxsize int) (*SQueueLite, error) {
+	return NewSQueueLiteWithTimeout(filenameOrConn, memory, maxsize, -1)
+}
+
+func NewSQueueLiteWithTimeout(filenameOrConn string, memory bool, maxsize int, peekTimeoutSec int) (*SQueueLite, error) {
 	if filenameOrConn == "" && !memory {
 		return nil, errors.New("either specify a filenameOrConn or pass memory=true")
 	}
 
-	notifyChan := make(chan int, 1)
+	notifyChan := make(chan int64, 1)
 
-	lq := &SQueueLite{
-		conn:    nil,
-		maxsize: maxsize,
-		notify:  notifyChan,
-	}
-
-	registerHook(lq.updateFunc)
-
-	conn, err := sql.Open("sqlite3_with_update_hook", buildDSN(filenameOrConn, memory))
+	conn, err := sql.Open("sqlite3", buildDSN(filenameOrConn, memory))
 	if err != nil {
 		return nil, err
 	}
+	driverConn, err := conn.Driver().Open(buildDSN(filenameOrConn, memory))
+	if err != nil {
+		return nil, err
+	}
+	sqliteConn, ok := driverConn.(*sqlite3.SQLiteConn)
+	if !ok {
+		return nil, errors.New("internal driver error")
+	}
 
-	lq.conn = conn
+	lq := &SQueueLite{
+		conn:           nil,
+		maxsize:        maxsize,
+		notify:         notifyChan,
+		peekTimeoutSec: peekTimeoutSec,
+	}
+	sqliteConn.RegisterUpdateHook(lq.updateFunc)
+
+	lq.conn = sqliteConn
 
 	if err = lq.init(); err != nil {
 		return nil, err
 	}
 
 	return lq, nil
-}
-
-func registerHook(callback func(mode int, database, table string, rowid int64)) {
-	sql.Register("sqlite3_with_update_hook",
-		&sqlite3.SQLiteDriver{
-			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
-				conn.RegisterUpdateHook(callback)
-				return nil
-			},
-		})
 }
 
 func buildDSN(filenameOrConn string, memory bool) string {
@@ -87,13 +90,10 @@ func buildDSN(filenameOrConn string, memory bool) string {
 }
 
 func (lq *SQueueLite) updateFunc(mode int, database, table string, rowid int64) {
-	//log.Printf("%+v,%+v,%+v,%+v", mode, database, table, rowid)
 	switch mode {
 	case sqlite3.SQLITE_INSERT:
-		// 	fallthrough
-		// case sqlite3.SQLITE_UPDATE:
 		go func() {
-			lq.notify <- mode
+			lq.notify <- rowid
 		}()
 	}
 }
@@ -112,33 +112,33 @@ func (lq *SQueueLite) init() error {
 		}
 	}()
 
-	if _, err = tx.Exec(`
+	if _, err = lq.conn.Exec(`
         CREATE TABLE IF NOT EXISTS Queue
         (
           Data      TEXT    NOT NULL,
           MessageID TEXT    NOT NULL,
           Status    INTEGER NOT NULL,
           inTime    INTEGER NOT NULL,
-          lockTime  INTEGER,
-          doneTime  INTEGER,
+          lockTime  INTEGER NOT NULL,
+          doneTime  INTEGER NOT NULL,
           PRIMARY KEY (MessageID)
-        )`); err != nil {
+        )`, []driver.Value{}); err != nil {
 		return err
 	}
 
-	if _, err = tx.Exec("CREATE INDEX IF NOT EXISTS SIdx ON Queue(Status)"); err != nil {
+	if _, err = lq.conn.Exec("CREATE INDEX IF NOT EXISTS SIdx ON Queue(Status)", []driver.Value{}); err != nil {
 		return err
 	}
 
 	if lq.maxsize > 0 {
-		if _, err = tx.Exec(fmt.Sprintf(`
+		if _, err = lq.conn.Exec(fmt.Sprintf(`
             CREATE TRIGGER IF NOT EXISTS maxsize_control
             BEFORE INSERT ON Queue
             WHEN (SELECT COUNT(*) FROM Queue WHERE Status = %d) >= %d
             BEGIN
                 SELECT RAISE(ABORT, 'Max queue length reached: %d');
             END;`,
-			READY, lq.maxsize, lq.maxsize)); err != nil {
+			READY, lq.maxsize, lq.maxsize), []driver.Value{}); err != nil {
 			return err
 		}
 	}
@@ -146,189 +146,159 @@ func (lq *SQueueLite) init() error {
 	return nil
 }
 
-func (lq *SQueueLite) Put(Data string) (*Message, error) {
+func (lq *SQueueLite) Put(Data string) error {
 	uuid, err := uuid.NewUUID()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	MessageID := uuid.String()
 	now := time.Now().UnixNano()
 	_, err = lq.conn.Exec(`
 			INSERT INTO
-			  Queue(  Data,  MessageID, Status,                 inTime, lockTime, doneTime )
-			VALUES ( ?, ?, ?, ?, NULL, NULL )
-			`, Data, MessageID, READY, now,
+			  Queue(  Data,  MessageID, Status, inTime, lockTime, doneTime )
+			VALUES ( ?, ?, ?, ?, 0, 0 )
+			`, []driver.Value{Data, MessageID, READY, now},
 	)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &Message{
-		Data,
-		MessageID,
-		READY,
-		now,
-		nil,
-		nil,
-	}, nil
+	return nil
 }
 
-// func (lq *SQueueLite) _pop_returning() (*Message, error) {
-// 	//with self.transaction(mode="IMMEDIATE"):
-// 	row := lq.conn.QueryRow(
-// 		`UPDATE
-//             Queue
-//         SET
-//             Status = ?, lockTime = ?
-//         WHERE
-//             rowid = (SELECT rowid FROM Queue WHERE Status = ? ORDER BY MessageID LIMIT 1) RETURNING *`,
-// 		LOCKED, time.Now().UnixNano(), READY,
-// 	)
-// 	message := Message{}
-// 	err := row.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	message.Status = LOCKED
-// 	message.lockTime = ptrInt64(time.Now().UnixNano())
-
-// 	return &message, nil
-// }
-
-// func ptrInt64(u int64) *int64 {
-// 	return &u
-// }
-
-// func (lq *SQueueLite) _pop_transaction() (*Message, error) {
-// 	//with self.transaction(mode="IMMEDIATE"):
-// 	row := lq.conn.QueryRow(
-// 		`SELECT
-//             *
-//         FROM
-//             Queue
-//         WHERE
-//             rowid = (SELECT rowid FROM Queue WHERE Status = ? ORDER BY MessageID LIMIT 1)`, READY,
-// 	)
-// 	message := Message{}
-// 	err := row.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	lockTime := time.Now().UnixNano()
-
-// 	_, err = lq.conn.Exec(`
-// 			UPDATE Queue SET
-// 			Status = ?
-// 			, lockTime = ?
-// 			WHERE MessageID = ? AND Status = ?`,
-// 		LOCKED, lockTime, message.MessageID, READY,
-// 	)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	message.Status = LOCKED
-// 	message.lockTime = ptrInt64(time.Now().UnixNano())
-
-// 	return &message, nil
-// }
-
 func (lq *SQueueLite) Peek() (*Message, error) {
-	row := lq.conn.QueryRow("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", READY)
-
-	message := Message{}
-	err := row.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime)
+	row, err := lq.conn.Query("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", []driver.Value{READY})
 	if err != nil {
 		return nil, err
 	}
-	message.Status = READY
-	message.lockTime = nil
-	message.doneTime = nil
+	defer row.Close()
+	message := Message{}
+	vals := []driver.Value{message.Data, message.MessageID, message.Status, message.inTime, message.lockTime, message.doneTime}
+	err = row.Next(vals)
+	if err != nil && err != io.EOF {
+		return nil, err
+	} else if err == io.EOF {
+		return nil, sql.ErrNoRows
+	}
+	message.Data = vals[0].(string)
+	message.MessageID = vals[1].(string)
+	message.Status = vals[2].(int64)
+	message.inTime = vals[3].(int64)
+	message.lockTime = vals[4].(int64)
+	message.doneTime = vals[5].(int64)
+
+	//erase notify if available
+	select {
+	case <-lq.notify:
+	default:
+	}
 
 	return &message, nil
 }
 
 func (lq *SQueueLite) PeekBlock() (*Message, error) {
-	row, err := lq.conn.Query("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", READY)
+	row, err := lq.conn.Query("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", []driver.Value{READY})
 	if err != nil {
 		return nil, err
 	}
-	for !row.Next() {
-		<-lq.notify
-		row, err = lq.conn.Query("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", READY)
-		if err != nil {
+	defer row.Close()
+
+	message := Message{}
+	vals := []driver.Value{message.Data, message.MessageID, message.Status, message.inTime, message.lockTime, message.doneTime}
+	err = row.Next(vals)
+	if err != nil {
+		if err == io.EOF {
+			//explicitly wait for a notification
+			if lq.peekTimeoutSec == -1 {
+				//no timeout, continue processing
+				<-lq.notify
+			} else {
+				select {
+				case <-lq.notify:
+					//continue processing
+				case <-time.After(time.Duration(int64(lq.peekTimeoutSec) * int64(time.Second))):
+					return nil, sql.ErrNoRows
+				}
+			}
+
+			row2, err := lq.conn.Query("SELECT * FROM Queue WHERE Status = ? ORDER BY inTime ASC LIMIT 1", []driver.Value{READY})
+			if err != nil {
+				return nil, err
+			}
+			defer row2.Close()
+			err = row2.Next(vals)
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+		} else {
 			return nil, err
 		}
 	}
-
-	message := Message{}
-	err = row.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime)
-	if err != nil {
-		return nil, err
-	}
-	message.Status = READY
-	message.lockTime = nil
-	message.doneTime = nil
+	message.Data = vals[0].(string)
+	message.MessageID = vals[1].(string)
+	message.Status = vals[2].(int64)
+	message.inTime = vals[3].(int64)
+	message.lockTime = vals[4].(int64)
+	message.doneTime = vals[5].(int64)
 
 	return &message, nil
 }
 
 func (lq *SQueueLite) Get(MessageID string) (*Message, error) {
-	row := lq.conn.QueryRow("SELECT * FROM Queue WHERE MessageID = ?", MessageID)
-
-	message := Message{}
-	err := row.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime)
+	row, err := lq.conn.Query("SELECT * FROM Queue WHERE MessageID = ?", []driver.Value{MessageID})
 	if err != nil {
 		return nil, err
 	}
-	message.Status = READY
-	message.lockTime = nil
-	message.doneTime = nil
+	defer row.Close()
+
+	message := Message{}
+	vals := []driver.Value{message.Data, message.MessageID, message.Status, message.inTime, message.lockTime, message.doneTime}
+	err = row.Next(vals)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	message.Data = vals[0].(string)
+	message.MessageID = vals[1].(string)
+	message.Status = vals[2].(int64)
+	message.inTime = vals[3].(int64)
+	message.lockTime = vals[4].(int64)
+	message.doneTime = vals[5].(int64)
 
 	return &message, nil
 }
 
-func (lq *SQueueLite) Done(MessageID string) (int, error) {
+func (lq *SQueueLite) Done(MessageID string) error {
 	now := time.Now().UnixNano()
 
-	result, err := lq.conn.Exec(`
+	_, err := lq.conn.Exec(`
         UPDATE Queue SET
             Status = ?
             , doneTime = ?
         WHERE MessageID = ?
-    `, DONE, now, MessageID)
+    `, []driver.Value{DONE, now, MessageID})
 
 	if err != nil {
-		return -1, err
+		return err
 	}
 
-	lastInsertID, err := result.LastInsertId()
-	if err != nil {
-		return -1, err
-	}
-
-	return int(lastInsertID), nil
+	return nil
 }
 
-func (lq *SQueueLite) MarkFailed(MessageID int) (int, error) {
+func (lq *SQueueLite) MarkFailed(MessageID int) error {
 	now := time.Now().UnixNano()
 
-	result, err := lq.conn.Exec(`
+	_, err := lq.conn.Exec(`
         UPDATE Queue SET
             Status = ?
             , doneTime = ?
         WHERE MessageID = ?
-    `, FAILED, now, MessageID)
+    `, []driver.Value{FAILED, now, MessageID})
 
 	if err != nil {
-		return -1, err
+		return err
 	}
 
-	lastInsertID, err := result.LastInsertId()
-	if err != nil {
-		return -1, err
-	}
-
-	return int(lastInsertID), nil
+	return nil
 }
 
 func (lq *SQueueLite) ListLocked(threshold_seconds int) ([]Message, error) {
@@ -339,79 +309,131 @@ func (lq *SQueueLite) ListLocked(threshold_seconds int) ([]Message, error) {
         WHERE
             Status = ?
             AND lockTime < ?
-    `, LOCKED, time.Now().UnixNano()-threshold_nanoseconds)
-
+    `, []driver.Value{LOCKED, time.Now().UnixNano() - threshold_nanoseconds})
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	messages := []Message{}
-	for rows.Next() {
-		var message Message
-		if err := rows.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime); err != nil {
-			if err != nil {
-				return nil, err
-			}
+
+	for {
+		message := Message{}
+		vals := []driver.Value{message.Data, message.MessageID, message.Status, message.inTime, message.lockTime, message.doneTime}
+		err = rows.Next(vals)
+		if err != nil && err != io.EOF {
+			return nil, err
+		} else if err == io.EOF {
+			break
 		}
+		message.Data = vals[0].(string)
+		message.MessageID = vals[1].(string)
+		message.Status = vals[2].(int64)
+		message.inTime = vals[3].(int64)
+		message.lockTime = vals[4].(int64)
+		message.doneTime = vals[5].(int64)
 		messages = append(messages, message)
 	}
 
 	return messages, nil
 }
 
-func (lq *SQueueLite) Retry(MessageID int) (int, error) {
-	result, err := lq.conn.Exec(`
+func (lq *SQueueLite) Retry(MessageID int) error {
+	_, err := lq.conn.Exec(`
         UPDATE Queue SET
             Status = ?
             , doneTime = NULL
         WHERE MessageID = ?
-    `, READY, MessageID)
+    `, []driver.Value{READY, MessageID})
 
 	if err != nil {
-		return -1, err
+		return err
 	}
+	return nil
+}
 
-	lastInsertID, err := result.LastInsertId()
+func (lq *SQueueLite) RetryFailed() error {
+	result, err := lq.conn.Exec(`
+        UPDATE Queue SET
+            Status = ?
+            , doneTime = NULL
+        WHERE Status = ?
+    `, []driver.Value{READY, FAILED})
 	if err != nil {
-		return -1, err
+		return err
 	}
 
-	return int(lastInsertID), nil
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	var i int64
+	for i = 0; i < rowsAffected; i++ {
+		go func(i int64) {
+			lq.notify <- i
+		}(i)
+	}
+
+	return nil
 }
 
 func (lq *SQueueLite) Qsize() (int, error) {
 	var count int
-	if err := lq.conn.QueryRow(`
+	rows, err := lq.conn.Query(`
         SELECT COUNT(*) FROM Queue
         WHERE Status NOT IN (?, ?)
-    `, DONE, FAILED).Scan(&count); err != nil {
+    `, []driver.Value{DONE, FAILED})
+	if err != nil {
 		return -1, err
 	}
+	defer rows.Close()
+	vals := []driver.Value{count}
+	err = rows.Next(vals)
+	if err != nil {
+		return -1, err
+	}
+	count = int(vals[0].(int64))
 	return count, nil
 }
 
 func (lq *SQueueLite) Empty() (bool, error) {
-	var cnt int
-	if err := lq.conn.QueryRow(`
+	var count int
+	rows, err := lq.conn.Query(`
         SELECT COUNT(*) as cnt FROM Queue WHERE Status = ?
-    `, READY).Scan(&cnt); err != nil {
-		return false, err
-	}
-	return cnt == 0, nil
-}
-
-func (lq *SQueueLite) Full() (bool, error) {
-	row := lq.conn.QueryRow("SELECT COUNT(*) as cnt FROM Queue WHERE Status = ?", READY)
-	var cnt int
-	err := row.Scan(&cnt)
+    `, []driver.Value{READY})
 	if err != nil {
 		return false, err
 	}
-	return cnt >= lq.maxsize, nil
+	defer rows.Close()
+	vals := []driver.Value{count}
+	err = rows.Next(vals)
+	if err != nil {
+		return false, err
+	}
+	count = int(vals[0].(int64))
+	return count == 0, nil
+}
+
+func (lq *SQueueLite) Full() (bool, error) {
+	var count int
+	rows, err := lq.conn.Query(fmt.Sprintf(`
+        SELECT COUNT(*) as cnt FROM Queue WHERE Status = %v
+    `, READY), []driver.Value{})
+	if err != nil {
+		return false, err
+	}
+	defer rows.Close()
+	vals := []driver.Value{count}
+	err = rows.Next(vals)
+	if err != nil {
+		return false, err
+	}
+	count = int(vals[0].(int64))
+	return count >= lq.maxsize, nil
 }
 
 func (lq *SQueueLite) Prune() error {
-	_, err := lq.conn.Exec("DELETE FROM Queue WHERE Status IN (?, ?)", DONE, FAILED)
+	_, err := lq.conn.Exec("DELETE FROM Queue WHERE Status IN (?, ?)", []driver.Value{DONE, FAILED})
 	if err != nil {
 		return err
 	}
@@ -419,7 +441,7 @@ func (lq *SQueueLite) Prune() error {
 }
 
 func (lq *SQueueLite) Vacuum() error {
-	_, err := lq.conn.Exec("VACUUM")
+	_, err := lq.conn.Exec("VACUUM", []driver.Value{})
 	return err
 }
 
@@ -427,31 +449,42 @@ func (lq *SQueueLite) Transaction(mode string) (func(), error) {
 	if mode != "DEFERRED" && mode != "IMMEDIATE" && mode != "EXCLUSIVE" {
 		return nil, fmt.Errorf("Transaction mode '%s' is not valid", mode)
 	}
-	_, err := lq.conn.Exec(fmt.Sprintf("BEGIN %s", mode))
+	_, err := lq.conn.Exec(fmt.Sprintf("BEGIN %s", mode), []driver.Value{})
 	if err != nil {
 		return nil, err
 	}
 	return func() {
 		if err != nil {
-			lq.conn.Exec("ROLLBACK")
+			lq.conn.Exec("ROLLBACK", []driver.Value{})
 		} else {
-			lq.conn.Exec("COMMIT")
+			lq.conn.Exec("COMMIT", []driver.Value{})
 		}
 	}, nil
 }
 
 func (lq *SQueueLite) String() string {
-	rows, err := lq.conn.Query("SELECT * FROM Queue LIMIT 3")
+	rows, err := lq.conn.Query("SELECT * FROM Queue LIMIT 3", []driver.Value{})
 	if err != nil {
 		return fmt.Sprintf("%T(Connection=%v)", lq, lq.conn)
 	}
 	defer rows.Close()
-	var displayItems []Message
-	for rows.Next() {
-		var message Message
-		if err := rows.Scan(&message.Data, &message.MessageID, &message.Status, &message.inTime, &message.lockTime, &message.doneTime); err != nil {
+	displayItems := []Message{}
+
+	for {
+		message := Message{}
+		vals := []driver.Value{message.Data, message.MessageID, message.Status, message.inTime, message.lockTime, message.doneTime}
+		err = rows.Next(vals)
+		if err != nil && err != io.EOF {
 			return fmt.Sprintf("%T(Connection=%v)", lq, lq.conn)
+		} else if err == io.EOF {
+			break
 		}
+		message.Data = vals[0].(string)
+		message.MessageID = vals[1].(string)
+		message.Status = vals[2].(int64)
+		message.inTime = vals[3].(int64)
+		message.lockTime = vals[4].(int64)
+		message.doneTime = vals[5].(int64)
 		displayItems = append(displayItems, message)
 	}
 	return fmt.Sprintf("%T(Connection=%v, items=%v)", lq, lq.conn, displayItems)
--- squeuelite_test.go
+++ squeuelite_test.go
@@ -2,99 +2,191 @@ package squeuelite
 
 import (
 	"database/sql"
+	"log"
+	"os"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func TestBaseTest(t *testing.T) {
-	queue, err := NewSQueueLite("test", true, 2)
+func TestBasicFunctionality(t *testing.T) {
+	//if on FS, delete old queue
+	defer os.Remove(".queue.db")
+	for _, env := range []bool{true, false} {
+		queue, err := NewSQueueLite(".queue", env, 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = queue.Put("Test1")
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = queue.Put("Test2")
+		if err != nil {
+			t.Fatal(err)
+		}
+		msg, err := queue.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if msg.Data != "Test1" {
+			t.Fatalf("First message payload should be Test1:%v", msg.Data)
+		}
+		msg, err = queue.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if msg.Data != "Test1" {
+			t.Fatalf("First message payload should be still Test1:%v", msg.Data)
+		}
+		err = queue.Done(msg.MessageID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		msg, err = queue.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if msg.Data != "Test2" {
+			t.Fatalf("Second message payload should be Test2:%v", msg.Data)
+		}
+		size, err := queue.Qsize()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if size != 1 {
+			t.Fatalf("Now only one message should be not DONE/FAILED:%v", size)
+		}
+		isEmpty, err := queue.Empty()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if isEmpty {
+			t.Fatal("Queue should not be empty")
+		}
+		isFull, err := queue.Full()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if isFull {
+			t.Fatal("Queue should not be full")
+		}
+		err = queue.Put("Test3")
+		if err != nil {
+			t.Fatal(err)
+		}
+		isFull, err = queue.Full()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !isFull {
+			t.Fatal("Queue should be full")
+		}
+		msg, err = queue.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if msg.Data != "Test2" {
+			t.Fatalf("Second message payload should be Test2:%v", msg.Data)
+		}
+		msg2, err := queue.Get(msg.MessageID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if msg.MessageID != msg2.MessageID {
+			t.Fatalf("Messages should be same from Peek and Get by msg id:%+v<->%+v", msg, msg2)
+		}
+		err = queue.Done(msg.MessageID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		msg, err = queue.Peek()
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = queue.Done(msg.MessageID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = queue.Prune()
+		if err != nil {
+			t.Fatal(err)
+		}
+		isEmpty, err = queue.Empty()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !isEmpty {
+			t.Fatal("Queue should be empty")
+		}
+		queue.Close()
+	}
+}
+
+func TestBasicPeakBlock(t *testing.T) {
+	queue, err := NewSQueueLite(".queue", true, 100)
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = queue.Put("Test1")
+
+	go func(queue *SQueueLite) {
+		time.Sleep(2 * time.Second)
+
+		err := queue.Put("Test")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(queue)
+
+	msg, err := queue.PeekBlock()
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = queue.Put("Test2")
+	if msg.Data != "Test" {
+		t.Fatalf("Should be Test:%v", msg.Data)
+	}
+}
+
+func TestLoad(t *testing.T) {
+	defer os.Remove(".queue.db")
+	queue, err := NewSQueueLite(".queue", false, 10)
 	if err != nil {
 		t.Fatal(err)
 	}
+	err = queue.Put("Test1!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = queue.Put("Test2!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = queue.Put("Test3!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = queue.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	queue, err = NewSQueueLite(".queue", false, 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	msg, err := queue.Peek()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if msg.Data != "Test1" {
-		t.Fatalf("First message payload should be Test1:%v", msg.Data)
+	if msg.Data != "Test1!" {
+		t.Fatalf("Should be Test1!:%v", msg.Data)
 	}
-	msg, err = queue.Peek()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if msg.Data != "Test1" {
-		t.Fatalf("First message payload should be still Test1:%v", msg.Data)
-	}
-	a, err := queue.Done(msg.MessageID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if a != 2 {
-		t.Fatalf("Last inserted id should be 2:%v", a)
-	}
-	msg, err = queue.Peek()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if msg.Data != "Test2" {
-		t.Fatalf("Second message payload should be Test2:%v", msg.Data)
-	}
-	size, err := queue.Qsize()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if size != 1 {
-		t.Fatalf("Now only one message should be not DONE/FAILED:%v", size)
-	}
-	isEmpty, err := queue.Empty()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if isEmpty {
-		t.Fatal("Queue should not be empty")
-	}
-	isFull, err := queue.Full()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if isFull {
-		t.Fatal("Queue should not be full")
-	}
-	_, err = queue.Put("Test3")
-	if err != nil {
-		t.Fatal(err)
-	}
-	isFull, err = queue.Full()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !isFull {
-		t.Fatal("Queue should be full")
-	}
-	msg, err = queue.Peek()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if msg.Data != "Test2" {
-		t.Fatalf("Second message payload should be Test2:%v", msg.Data)
-	}
-	msg2, err := queue.Get(msg.MessageID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if msg.MessageID != msg2.MessageID {
-		t.Fatalf("Messages should be same from Peek and Get by msg id:%+v<->%+v", msg, msg2)
-	}
-	_, err = queue.Done(msg.MessageID)
+	err = queue.Done(msg.MessageID)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -102,55 +194,104 @@ func TestBaseTest(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = queue.Done(msg.MessageID)
+	if msg.Data != "Test2!" {
+		t.Fatalf("Should be Test1!:%v", msg.Data)
+	}
+	err = queue.Done(msg.MessageID)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	err = queue.Prune()
+	msg, err = queue.Peek()
 	if err != nil {
 		t.Fatal(err)
 	}
-	isEmpty, err = queue.Empty()
+	if msg.Data != "Test3!" {
+		t.Fatalf("Should be Test1!:%v", msg.Data)
+	}
+	err = queue.Done(msg.MessageID)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if !isEmpty {
+	isempty, err := queue.Empty()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !isempty {
 		t.Fatal("Queue should be empty")
+	}
+	err = queue.Close()
+	if err != nil {
+		t.Fatal(err)
 	}
 }
 
-func TestSQueueLite_Done(t *testing.T) {
-	type fields struct {
-		conn    *sql.DB
-		maxsize int
+func TestMultithreading(t *testing.T) {
+	os.Remove(".queue.db")
+	os.Remove(".queue.db-journal")
+	defer os.Remove(".queue.db")
+	defer os.Remove(".queue.db-journal")
+	queue, err := NewSQueueLiteWithTimeout(".queue", false, 100000, 2)
+	if err != nil {
+		t.Fatal(err)
 	}
-	type args struct {
-		MessageID string
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    int
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			lq := &SQueueLite{
-				conn:    tt.fields.conn,
-				maxsize: tt.fields.maxsize,
+	wg := sync.WaitGroup{}
+	//producers
+	for i := 0; i < 10; i++ {
+		go func() {
+			for j := 0; j < 10; j++ {
+				content := uuid.New().String()
+				err = queue.Put(content)
+				if err != nil {
+					t.Fatal(err)
+				}
 			}
-			got, err := lq.Done(tt.args.MessageID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("SQueueLite.Done() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("SQueueLite.Done() = %v, want %v", got, tt.want)
-			}
-		})
+			log.Printf("Producer done:%v", i)
+			wg.Done()
+
+		}()
+		wg.Add(1)
 	}
+	//consumers PeekingBlock
+	for i := 0; i < 5; i++ {
+		go func() {
+			for {
+				msg, err := queue.PeekBlock()
+				if err != nil {
+					if err == sql.ErrNoRows {
+						wg.Done()
+						log.Printf("Block Peek Consumer done:%v", i)
+						break
+					}
+				}
+				//log.Printf("BlockPeeked msg:%v from cons:%v", msg.MessageID, i)
+				err = queue.Done(msg.MessageID)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+		}()
+		wg.Add(1)
+	}
+	//consumers Peeking
+	for i := 0; i < 5; i++ {
+		go func() {
+			for {
+				msg, err := queue.Peek()
+				if err != nil {
+					if err == sql.ErrNoRows {
+						wg.Done()
+						log.Printf("Peek Consumer done:%v", i)
+						break
+					}
+				}
+				//log.Printf("Peeked msg:%v from cons:%v", msg.MessageID, i)
+				err = queue.Done(msg.MessageID)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+		}()
+		wg.Add(1)
+	}
+	wg.Wait()
 }
